34465066493eea4d15085f6992a01a28d0c29aca
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2019-08-09 17:17:26 +0530

    Adding visibility props and updating style

---
+++ src/Rect/StyledRect.js
@@ -0,0 +1,114 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  position: absolute;
+  border: 1px solid #9C27B0;
+
+  .circle {
+    position: absolute;
+    width: 7px;
+    height: 7px;
+    background: white;
+    border: 1px solid white;
+    border-radius: 50%;
+    -moz-box-shadow: 0px 0px 5px 1px #ccc;
+    -webkit-box-shadow: 0px 0px 5px 1px #ccc;
+    box-shadow: 0px 0px 5px 1px #ccc;
+  }
+
+  .resizable-handler {
+    position: absolute;
+    width: 14px;
+    height: 14px;
+    cursor: pointer;
+    z-index: 1;
+
+    &.tl,
+    &.t,
+    &.tr {
+      top: -7px;
+    }
+
+    &.tl,
+    &.l,
+    &.bl {
+      left: -7px;
+    }
+
+    &.bl,
+    &.b,
+    &.br {
+      bottom: -7px;
+    }
+
+    &.br,
+    &.r,
+    &.tr {
+      right: -7px;
+    }
+
+    &.l,
+    &.r {
+      margin-top: -7px;
+    }
+
+    &.t,
+    &.b {
+      margin-left: -7px;
+    }
+  }
+
+  .rotate {
+    background-color: #fff;
+    border-radius: 50%;
+    position: absolute;
+    left: 50%;
+    top: -30px;
+    width: 22px;
+    height: 22px;
+    margin-left: -9px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    -moz-box-shadow: 0px 0px 5px 1px #ccc;
+    -webkit-box-shadow: 0px 0px 5px 1px #ccc;
+    box-shadow: 0px 0px 5px 1px #ccc;
+  }
+
+  .t,
+  .tl,
+  .tr {
+    top: -3px;
+  }
+
+  .b,
+  .bl,
+  .br {
+    bottom: -3px;
+  }
+
+  .r,
+  .tr,
+  .br {
+    right: -3px;
+  }
+
+  .tl,
+  .l,
+  .bl {
+    left: -3px;
+  }
+
+  .l,
+  .r {
+    top: 50%;
+    margin-top: -3px;
+  }
+
+  .t,
+  .b {
+    left: 50%;
+    margin-left: -3px;
+  }
+`
